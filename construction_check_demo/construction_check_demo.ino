9e6b41f79d694ea23b8cca45e6a346fea3ed8f7d
Anthony Tenggoro <anthony.tenggoro@gmail.com>
2016-03-14 22:43:10 -0400

    Added Pinout Comments For Sensors and Fixed Motor Addresses to Go Forward

--- construction_check_demo/construction_check_demo.ino
+++ construction_check_demo/construction_check_demo.ino
@@ -1,10 +1,24 @@
 /* 
-This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
-It won't work with v1.x motor shields! Only for the v2's with built in PWM
-control
+********************************************** 
+        Anthony Sensor Wiring Pinout
+**********************************************
 
-For use with the Adafruit Motor Shield v2 
----->  http://www.adafruit.com/products/1438
+************* Ultrasonic Sensors *************
+* All pins are Digital I/O for Ultrasonic
+* 
+* Front Face Left (Echo = 38 Trig = 39)
+* Front Face Right (Echo = 40 Trig = 41)
+* Left Face Front Left (Echo = 24 Trig = 25)
+* Left Face Bottom Left (Echo = 22 Trig = 23)
+* Right Face Front Right (Echo = 50 Trig = 51)
+* Right Face Bottom Right (Echo = 52 Trig = 53)
+* 
+*************   MPU 6050 Wiring  *************
+* SCL = Digital I/O (Communication) Pin 21
+* SDA = Digital I/O (Communication) Pin 20
+* INT = Digital I/O (PWM)           Pin 2
+* 
+* ********************************************
 */
 
 #include <Wire.h>
@@ -16,23 +30,23 @@ Adafruit_MotorShield AFMSbot(0x61); // Rightmost jumper closed
 Adafruit_MotorShield AFMStop(0x60); // Default address, no jumpers
 
 // Define Motors
-Adafruit_StepperMotor *myStepperFL = AFMStop.getStepper(200, 1); //Front Left
-Adafruit_StepperMotor *myStepperFR = AFMStop.getStepper(200, 2); // Front Right
-Adafruit_StepperMotor *myStepperBL = AFMSbot.getStepper(200, 2); //Bottom Left
-Adafruit_StepperMotor *myStepperBR = AFMSbot.getStepper(200, 1); //Bottom Right
+Adafruit_StepperMotor *myStepperFR = AFMStop.getStepper(200, 1);  //Front Right
+Adafruit_StepperMotor *myStepperBR= AFMStop.getStepper(200, 2);   // Bottom Right
+Adafruit_StepperMotor *myStepperFL = AFMSbot.getStepper(200, 1);  //Front Left
+Adafruit_StepperMotor *myStepperBL = AFMSbot.getStepper(200, 2);  //Bottom Left
 
 
 void setup() {
   while (!Serial);
-  Serial.begin(9600);           // set up Serial library at 9600 bps
-  AFMSbot.begin(); // Start the bottom shield
-  AFMStop.begin(); // Start the top shield
+  Serial.begin(9600);   // set up Serial library at 9600 bps
+  AFMSbot.begin();      // Start the bottom shield
+  AFMStop.begin();      // Start the top shield
   TWBR = ((F_CPU /200000l) - 16) / 2; // Change the i2c clock to 200KHz so motor can run faster (400KHz is fastest it can go)
 }
 
 int i;
 void loop() {
-    
+    /*
     delay(500);
     asyncGoForward(162,0);
     delay(100);
@@ -49,7 +63,8 @@ void loop() {
     asyncGoForward(162,0);
     delay(100);
     asyncGoLeft(162,0);
-    delay(100);
+    delay(100);*/
+    asyncGoForward(162,0);
 }
 
 void asyncGoForward(int steps, int delayMs){
@@ -88,7 +103,7 @@ void asyncGoRight(int steps, int delayMs){
   
 }
 
-void asyncGoBack(int steps){
+void asyncGoBack(int steps, int delayMs){
     i = 0;
     while(i < steps){
     myStepperFL->onestep(FORWARD, DOUBLE);
